c76052b3b376d33cba135a4eadcce9653c913f5f
Héctor Reyes <hectorreyesmanrique@gmail.com>
2020-10-02 10:21:11 -0500

    Add Activity 6

---
+++ main.c
@@ -0,0 +1,94 @@
+/*  Actividad 6: Sockets y señales
+    Curso: Programación avanzada
+    Profesor: Vicente Cubells Nonell
+    Autores:
+        Daniela Vignau León (A01021698)
+        Héctor Alexis Reyes Manrique (A01339607)
+    Fecha: 2 de octubre de 2020
+*/
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+
+#define TCP_PORT 8000
+
+char interruption;
+int fd;
+int semaphoreId = 0;
+
+void handler(int signal) {
+    if (signal == SIGINT) {
+        interruption = 'I';
+    } else if (signal == SIGTSTP) {
+        interruption = 'R';
+    }
+    write(fd, &interruption, sizeof(char));
+}
+
+int main(int argc, const char * argv[]) {    
+    struct sigaction sa;
+    sa.sa_handler = handler;
+    sigaction(SIGTSTP, &sa, 0);
+    sigaction(SIGINT, &sa, 0);
+
+    struct sockaddr_in address;
+    char buffer;
+    int connect = 1, console, semaphore; 
+    size_t receivedData, sentData;
+    pid_t pid;
+    
+    console = socket(PF_INET, SOCK_STREAM, 0);
+    inet_aton(argv[1], &address.sin_addr);
+    address.sin_port = htons(TCP_PORT);
+    address.sin_family = AF_INET;
+    
+    bind(console, (struct sockaddr *) &address, sizeof(address));
+    listen(console, 10);
+    sentData = sizeof(address);
+
+    while (connect) {
+        semaphore = accept(console, (struct sockaddr *) &address, &sentData);
+        semaphoreId += 1;
+        pid = fork();
+        if (pid == 0) {
+            connect = 0;
+        }
+    }
+
+    if (pid == 0) {
+        close(console);
+        if (semaphore >= 0) {
+            fd = semaphore;
+            while ((receivedData = read(semaphore, &buffer, sizeof(buffer)))) {
+                if (receivedData == 1) {
+                    printf("Semaphore %d changed state to ", semaphoreId);
+                    switch (buffer) {
+                        case 'R':
+                            printf("RED\n");
+                            break;
+                        case 'G':
+                            printf("GREEN\n");
+                            break;
+                        case 'I':
+                            printf("INTERMITTENT\n");
+                            break;
+                    }
+                }
+                buffer = '\0';
+            }
+        }
+        close(semaphore);
+    } else if (pid > 0) {
+        while (wait(NULL) != -1);
+        close(console);
+    }
+    
+    return 0;
+}
---
+++ semaphore.c
@@ -0,0 +1,117 @@
+/*  Actividad 6: Sockets y señales
+    Curso: Programación avanzada
+    Profesor: Vicente Cubells Nonell
+    Autores:
+        Daniela Vignau León (A01021698)
+        Héctor Alexis Reyes Manrique (A01339607)
+    Fecha: 2 de octubre de 2020
+*/
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <getopt.h>
+#include <string.h>
+
+#define TCP_PORT 8000
+
+int nextPid;
+char currState = 'R';
+char lastState;
+
+int fd;
+
+void alarmHandler(int signal) {
+    kill(nextPid, SIGUSR1);
+    currState = 'R';
+    printf("State: %c\n", currState);
+    write(fd, &currState, sizeof(char));
+}
+
+void handler(int signal) {
+    struct sigaction sa;
+    sa.sa_handler = alarmHandler;       
+    currState = 'G';
+    printf("State: %c\n", currState);
+    write(fd, &currState, sizeof(char));
+    alarm(8);
+    sigaction(SIGALRM, &sa, 0);
+}
+
+int main(int argc, char * argv[]) {    
+    struct sigaction sa;
+    sa.sa_handler = handler;
+    sigaction(SIGUSR1, &sa, 0);
+
+    struct sockaddr_in address;
+    char buffer, receivedSignal = '\0', *value;
+    int arg, flag = 0, isFirst, semaphore;
+    ssize_t receivedData, sentData;
+
+    while ((arg = getopt(argc, argv, "f")) != -1) {
+        switch (arg) {
+            case 'f':
+                isFirst = 1;
+                break;
+            default:
+                abort();
+        }
+    }
+
+    semaphore = socket(PF_INET, SOCK_STREAM, 0);
+    fd = semaphore;
+
+    if (isFirst == 1) {
+        inet_aton(argv[2], &address.sin_addr);
+    } else {
+        inet_aton(argv[1], &address.sin_addr);
+    }
+
+    address.sin_port = htons(TCP_PORT);
+    address.sin_family = AF_INET;
+    sentData = connect(semaphore, (struct sockaddr *) &address, sizeof(address));
+
+    if (sentData == 0)  {
+        printf("PID: %d\nEnter the PID of the semaphore on the right: ", getpid());
+        scanf("%d", &nextPid);
+        printf("Sending signals to sempahore with PID %d\n", nextPid);
+        
+        if (isFirst == 1) {
+            printf("State: %c\n", currState);
+            kill(nextPid, SIGUSR1);
+            isFirst = 0;
+        }
+
+        while ((receivedData = read(semaphore, &buffer, sizeof(buffer)))) {
+            if (receivedData == 1) { 
+                if (receivedSignal != buffer) {
+                    receivedSignal = buffer;
+                    if (flag == 0) {
+                        lastState = currState;
+                        flag = 1;
+                    }
+                    currState = receivedSignal;
+                    printf("State: %c\n", currState);
+                    write(fd, &currState, sizeof(char));
+                    alarm(0);
+                } else {
+                    receivedSignal = '\0';
+                    currState = lastState;
+                    flag = 0;
+                    if (currState == 'G') {
+                        printf("\nRestarting semaphores...\n\n");
+                        raise(SIGUSR1);
+                    }
+                }
+            }
+        }
+    }
+
+    close(semaphore);
+    
+    return 0;
+}
